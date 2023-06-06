c50b889e25ab0411fcecf978aacc3ea936c012f9
vluvl <vladmuscoi@gmail.com>
2023-06-06 13:01:10 +0200

    Added a data analyzer python script that: ->Accepts the name/path to the folder in which the data is stored ->Can sort the data if prompted at the start ->Will display a plot showing the distribution of data available ->Will print the last 10 elements of the list Added links in the README file to the data set and the notebook that we can use for reference

---
+++ ExtractData.py
@@ -0,0 +1,39 @@
+import os
+import matplotlib.pyplot as plt
+#import seaborn as sn
+
+# import cv2
+from random import randint
+
+import numpy as np
+
+if __name__ == '__main__':
+    gems, count = [], []  # names of classes, count of images for each class
+    folder = input("File directory to be scanned:")  # filepath
+    sort = input("Sort classes? Y/N ")  # if the data should be sorted
+    for root, dirs, files in os.walk(folder):
+        f = os.path.basename(root)
+        if len(files) > 0:  # if there are files in a folder
+            count.append(len(files))  # append their number
+            if f not in gems:
+                gems.append(f)      # add the name of the class if it is not yet present
+    type_count = len(gems)  # total number of classes
+    classes = list(zip(gems, count[type_count:], count[0:type_count]))  # creating a list that will be sorted by the amount of training data
+
+    if sort == "Y":
+        classes.sort(key=lambda x: x[1])    # sort the list
+
+    f, ax = plt.subplots(figsize=(10, 7))
+    trainCount = []
+    testCount = []
+    for i in classes[0:type_count]: # extracting relevent information from the list
+        trainCount.append(i[1])
+        testCount.append(i[2])
+    # plot the data
+    plt.bar(range(type_count), trainCount[0:type_count], label='Train data')
+    plt.bar(range(type_count), testCount[0:type_count], label='Test data')
+    # print last 10 elements
+    print(classes[type_count-10:type_count])
+    ax.grid()
+    ax.legend(fontsize=12)
+    plt.show()
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # NN_Project
+
+Link to dataset: https://www.kaggle.com/datasets/lsind18/gemstones-images
+Link to "inspiration": https://www.kaggle.com/code/lsind18/gemstones-multiclass-classification-cnn
